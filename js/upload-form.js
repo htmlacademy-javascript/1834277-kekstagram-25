33a844e6ad46e1ba8bd636e6974e6197cc9fe58c
Дмитрий Бондарь <dmitbond@yahoo.com>
2022-04-23 18:09:59 +0300

    validation messages

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,8 +1,13 @@
 import {body} from './full-screen.js';
 
+const COMMENTS_MAX_LENGTH = 140;
+const HASHTAG_MAX_LENGTH = 20;
+const HASHTAG_MAX_NUMBER = 5;
 const form = document.querySelector('#upload-select-image');
 const hashtag = form.querySelector('.text__hashtags');
 const textDescription = form.querySelector('.text__description');
+const checkRepeatHashtag = (arg) => arg.some((val, index, arr) => arr.indexOf(val) !== index);
+const checkLengthHashtag = (arg) => arg.some((value) => value.length > HASHTAG_MAX_LENGTH);
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
@@ -16,40 +21,41 @@ function validateHashtags () {
   const re = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}$/;
   const hashtagLowerCase = hashtag.value.toLowerCase();
   const hashtagsArray = hashtagLowerCase.split(' ');
-  const checkRepeatHashtag = (arg) => arg.some((val, index, arr) => arr.indexOf(val) !== index);
 
-  if (hashtagsArray.length > 5) {
+  if (hashtagsArray.length > HASHTAG_MAX_NUMBER) {
+    hashtag.setCustomValidity(`Хэштегов должно быть не больше ${HASHTAG_MAX_NUMBER}`);
     return false;
   }
 
-  if (hashtag === '') {
-    return true;
+  if (checkLengthHashtag(hashtagsArray)) {
+    hashtag.setCustomValidity(`Хэш-тег не должен превышать ${HASHTAG_MAX_LENGTH} символов`);
+    return false;
   }
 
-  for (let i = 0; i < hashtagsArray.length; i++) {
-    if (re.test(hashtagsArray[i]) === false) {
-      return false;
-    }
+  if (hashtagsArray.every((element) => re.test(element)) === false) {
+    hashtag.setCustomValidity('Хэштеги должны состоять из решетки, букв и цифр');
+    return false;
   }
 
   if (checkRepeatHashtag(hashtagsArray)) {
+    hashtag.setCustomValidity('Хэштеги не должны повторяться');
     return false;
   }
-
+  hashtag.setCustomValidity('');
   return true;
 }
 
 function validateTextDescription () {
   const textDescriptionArray = textDescription.value.split('');
-  if (textDescriptionArray.length <= 140) {
+  if (textDescriptionArray.length <= COMMENTS_MAX_LENGTH) {
     return true;
   }
   return false;
 }
 
-pristine.addValidator(hashtag, validateHashtags);
+pristine.addValidator(hashtag, validateHashtags, 'что-то не красивое');
 
-pristine.addValidator(textDescription, validateTextDescription);
+pristine.addValidator(textDescription, validateTextDescription,'что-то не красивое');
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
@@ -58,32 +64,11 @@ form.addEventListener('submit', (evt) => {
 
 const uploadFile = document.querySelector('#upload-file');
 const imgUpload = document.querySelector('.img-upload__overlay');
+
 uploadFile.addEventListener('change', () => {
   imgUpload.classList.remove('hidden');
   body.classList.add('modal-open');
 });
-
-/* const imgUploadPreview = document.querySelector('.img-upload__preview > img'); */
-
-/* uploadFile.addEventListener("change", function () {
-  getImgData();
-}); */
-
-/* function getImgData() {
-  const files = uploadFile.files[0];
-  if (files) {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(files);
-    fileReader.addEventListener("load", function () {
-      imgUploadPreview.style.display = "block";
-      imgUploadPreview.innerHTML = '<img src="' + this.result + '" />';
-    });
-  }
-} */
-
-/* imgUploadPreview.src = URL.createObjectURL(uploadFile.target.files[0]); */
-/* console.log(uploadFile.files[0]); */
-/* console.log(URL.createObjectURL(uploadFile.target.files[0])); */
 
 const uploadCancelButton = document.querySelector('#upload-cancel');
 
